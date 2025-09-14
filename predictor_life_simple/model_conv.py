c2d368d8de64909bcf6817f583cf092440d14239
hrjtju <gravitastju@gmail.com>
2025-09-14 11:05:19 +0800

    update ker visualization

--- predictor_life_simple/model_conv.py
+++ predictor_life_simple/model_conv.py
@@ -54,21 +54,19 @@ class SimpleCNNTiny(nn.Module):
 
     def __init__(self):
         super().__init__()
-        self.conv1 = nn.Conv2d(2, 2, kernel_size=3, stride=1, padding=1, padding_mode="circular")
-        self.bn1 = nn.BatchNorm2d(2)
+        self.conv1 = nn.Conv2d(2, 1, kernel_size=3, stride=1, padding=1, padding_mode="circular")
+        self.bn1 = nn.BatchNorm2d(1)
         self.act1 = nn.ReLU()
-        self.conv2 = nn.Conv2d(2, 2, kernel_size=3, stride=1, padding=1, padding_mode="circular")
-        self.bn2 = nn.BatchNorm2d(2)
+        self.conv2 = nn.Conv2d(1, 1, kernel_size=3, stride=1, padding=1, padding_mode="circular")
+        self.bn2 = nn.BatchNorm2d(1)
         self.act2 = nn.ReLU()
-        self.conv3 = nn.Conv2d(2, 2, kernel_size=3, stride=1, padding=1, padding_mode="circular")
+        self.conv3 = nn.Conv2d(1, 2, kernel_size=3, stride=1, padding=1, padding_mode="circular")
         # 可选：self.bn3 = nn.BatchNorm2d(2)
 
     def forward(self, x: Float[Array, "batch 2 w h"]) -> Float[Array, "batch 2 w h"]:
         x = self.act1(self.bn1(self.conv1(x)))
         x = self.act2(self.bn2(self.conv2(x)))
         x = self.conv3(x)
-        # 如果用于分类，建议不加 sigmoid
-        # x = torch.sigmoid(x)
         return x
 
 class MultiScale(nn.Module):
